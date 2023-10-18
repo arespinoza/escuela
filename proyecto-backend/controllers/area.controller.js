6625bc328e3861697938353a4a7b6d8c291ab076
arespinoza <alfredo_espi@hotmail.com>
2023-10-18 18:52:22 -0300

    populando destinatarios

--- proyecto-backend/controllers/area.controller.js
+++ proyecto-backend/controllers/area.controller.js
@@ -5,7 +5,7 @@ const areaCtrl = {}
 areaCtrl.getArea = async (req, res) => {
     //console.log(req.params.id);
     const aarea = await Area.findById(req.params.id)
-    .populate('responsables')
+    .populate('responsables').populate('anuncios')
     res.json(aarea);
     //console.log(aarea);
 }
